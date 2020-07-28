f2b714aa5e42a0e1bc2a260809c642162745d7bc
Glitch (hello-webpage) <none>
2020-07-28 17:45:34 +0000

    👨‍❤️‍💋‍👨💂 Checkpoint ./script.js:18049447/485

--- script.js
+++ script.js
@@ -2,17 +2,23 @@ function cartFunction()
 {
  window.location = "cart.html";
 }
-var numElements = goodTeams.length;
 var matchFound = false;
-var teamCheck = "patriots";
-var goodTeams = ["Packers", "patriots", "titans", "chiefs"];
-for  (var i = 0; i <= 3; i++){
-if (teamCheck === goodTeams[i]){
-  matchFound = true
-  alert("You got it")
-  break;
-} 
-}
-if (matchFound === false) {
-alert("No Match")
+// var numElements = goodTeams.length;
+// var teamCheck = "chiefs";
+// var goodTeams = ["Packers", "patriots", "titans", "chiefs"];
+// for  (var i = 0; i < goodTeams.length; i++){
+// if (teamCheck === goodTeams[i]){
+//   matchFound = true
+//   alert("You got it")
+//   break;
+// } 
+// }
+// if (matchFound === false) {
+// alert("No Match")
+// }
+var peopleLike = ["kasey","zach","diego","taylor","cole"];
+var peopleHate = ["abby","marissa","bella","penelope","tarzan"];
+var full = []
+for (var i = 0; i <= peopleLike.length; i++){
+for (var j = 0; j <= peopleHate.length; i ++)
 }
