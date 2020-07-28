190fda17dc15381728405283fad026accae1d389
Glitch (hello-webpage) <none>
2020-07-28 17:34:14 +0000

    🏑👋 Checkpoint ./script.js:18049447/294

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ function cartFunction()
 {
  window.location = "cart.html";
 }
-var teamCheck = "panthers"
+var numElements = goodTeams.length;
+var matchFound = false;
+var teamCheck = "patriots";
 var goodTeams = ["Packers", "patriots", "titans", "chiefs"];
 for  (var i = 0; i <= 3; i++){
 if (teamCheck === goodTeams[i]){
+  matchFound = true
   alert("You got it")
+  break;
 } 
+}
+if (matchFound === false) {
+alert("No Match")
 }
